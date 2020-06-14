48522ecef5dcfaddb477e25b51f4040442a9284e
thabotitus <hello@thabotitus.co.za>
2020-06-14 11:05:18 +0200

    fix codacy issues

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,9 @@
 (function(){
   document.addEventListener("DOMContentLoaded", function(event) {
-    var loader = document.querySelector('.jca-loader');
-    var content = document.querySelector('.jca-layout');
+    var loader = document.querySelector(".jca-loader");
+    var content = document.querySelector(".jca-layout");
 
-    loader.classList.add('d-none');
-    content.classList.remove('d-none');
+    loader.classList.add("d-none");
+    content.classList.remove("d-none");
   });
-})();
+}());
