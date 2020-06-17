f3e4d3ca90a6ef968c53e4319e5dbcc7921ef20f
thabotitus <hello@thabotitus.co.za>
2020-06-17 22:05:41 +0200

    linting

--- src/js/main.js
+++ src/js/main.js
@@ -9,31 +9,31 @@
       loader.classList.add("d-none");
       content.classList.remove("d-none");
     });
-  }
+  };
 
   window.JCA.inputMasks = () => {
-    const visaInputs = document.querySelectorAll('[data-mask="visa"');
-    const masterCardInputs = document.querySelectorAll('[data-mask="mastercard"');
-    const cvvInputs = document.querySelectorAll('[data-mask="cvv"');
+    const visaInputs = document.querySelectorAll("[data-mask='visa'");
+    const masterCardInputs = document.querySelectorAll("[data-mask='mastercard'");
+    const cvvInputs = document.querySelectorAll("[data-mask='cvv'");
 
     visaInputs.forEach((element) => {
       new IMask(element, {
-        mask: '4000 0000 0000 0000'
+        mask: "4000 0000 0000 0000"
       });
     });
 
     masterCardInputs.forEach((element) => {
       new IMask(element, {
-        mask: '5000 0000 0000 0000'
+        mask: "5000 0000 0000 0000"
       });
     });
 
     cvvInputs.forEach((element) => {
       new IMask(element, {
-        mask: '000'
+        mask: "000"
       });
     });
-  }
+  };
 
   window.JCA.loader();
   window.JCA.inputMasks();
