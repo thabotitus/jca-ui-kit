046c1df402ea168d772149637f71cb9aeed52d63
Thabo Titus <thabotitus@gmail.com>
2022-06-06 08:09:26 +0200

    wip

--- build/scripts.js
+++ build/scripts.js
@@ -5,20 +5,29 @@ import notify from 'gulp-notify';
 import webpack from 'webpack-stream';
 import { WEBPACK_CONFIG } from '../webpack.config.js';
 import browserSync from 'browser-sync';
-import { DISTRIBUTION_FOLDERS } from './config.js';
+import { DISTRIBUTION_FOLDERS, INPUT_FOLDERS } from './config.js';
+import gulpEsbuild  from "gulp-esbuild";
 
 const browser = browserSync.has('jca-ui')
   ? browserSync.get('jca-ui')
   : browserSync.create('jca-ui');
 
-gulp.task(
-	'build:javascript',
-	function(done){
-		return gulp.src('/')
-			.pipe(plumber({
-				errorHandler: notify.onError("Error: <%= error.message %>")
-			}))
-			.pipe(webpack(WEBPACK_CONFIG), webpack)
-			.pipe(gulp.dest(`${DISTRIBUTION_FOLDERS.ROOT}/${DISTRIBUTION_FOLDERS.JS}`))
-			.pipe(browser.stream())
-});
+gulp.task('build:javascript', function () {
+	return gulp
+	  .src([`${INPUT_FOLDERS.JS}/app.js`, `${INPUT_FOLDERS.JS}/custom.js`])
+	  .pipe(
+		plumber({
+		  errorHandler: notify.onError("Error: <%= error.message %>"),
+		})
+	  )
+	  .pipe(gulpEsbuild({
+		outdir: `./`,
+		entryNames: './[name].min',
+		bundle:     true,
+		minify:     true,
+		sourcemap:  false,
+		metafile:   true
+	  }))
+	  .pipe(gulp.dest(`${DISTRIBUTION_FOLDERS.ROOT}/${DISTRIBUTION_FOLDERS.JS}`))
+	  .pipe(browser.stream());
+  });
